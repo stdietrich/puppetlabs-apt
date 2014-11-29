466b97dd67f89e3c0fa6931e2b5c38ee166b782f
Stefan Dietrich <stefan.dietrich@desy.de>
2014-11-29 22:29:35 +0100

    Fixu

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,9 +1,9 @@
 class apt::params {
-  $root                 = '/etc/apt'
-  $provider             = '/usr/bin/apt-get'
-  $sources_list_d       = "${root}/sources.list.d"
-  $apt_conf_d           = "${root}/apt.conf.d"
-  $preferences_d        = "${root}/preferences.d"
+  $root           = '/etc/apt'
+  $provider       = '/usr/bin/apt-get'
+  $sources_list_d = "${root}/sources.list.d"
+  $apt_conf_d     = "${root}/apt.conf.d"
+  $preferences_d  = "${root}/preferences.d"
 
   case $::lsbdistid {
     'ubuntu', 'debian': {
